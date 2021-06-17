b08660c083a04d67773eda51d3b7629710ed361e
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-17 16:27:35 +0900

    editへのルーティングの追加

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
 import AdminHome from "./components/adminHome/AdminHome";
 import OutputNew from "./components/outputNew/OutputNew";
+import OutputEdit from "./components/outputEdit/OutputEdit";
 
 const Routing = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/admin" component={AdminHome} />
-      <Route exact path="/output/new" component={OutputNew} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/admin" component={AdminHome} />
+        <Route exact path="/output/new" component={OutputNew} />
+        <Route path="/blog/edit/:blogId" component={OutputEdit} />
+      </Switch>
     </BrowserRouter>
   );
 };
