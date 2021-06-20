30fa8a18e73f4240c11a0a85eef7c3d93dd2eaf1
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-20 13:47:41 +0900

    blogの全件取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Routing from "./Routing";
 import styles from "./App.module.scss";
+import { fetchBlogs } from "./features/blog/blogSlice";
+import { AppDispatch } from "./app/store";
 
 const App: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    const getData = () => {
+      dispatch(fetchBlogs());
+    };
+    getData();
+  }, []);
   return (
     <div className={styles.root}>
       <Routing />
