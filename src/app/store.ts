cda15fa4ec7e2193b9a0d9324f733d11e4b11111
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-25 10:31:44 +0900

    userReducerの読み込み

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import blogReducer from "../features/blog/blogSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     blog: blogReducer,
+    user: userReducer,
   },
 });
 
