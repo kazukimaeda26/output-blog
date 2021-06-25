221ef9cbdf3b1f8275a2564bd6d440f4cd2c1a04
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-25 09:55:21 +0900

    管理者ログイン画面へのルーティング作成

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
-import AdminHome from "./components/adminHome/AdminHome";
+import AdminAuth from "./components/adminAuth/AdminAuth";
 import BlogNew from "./components/blogNew/BlogNew";
 import BlogEdit from "./components/blogEdit/BlogEdit";
 
@@ -10,7 +10,7 @@ const Routing = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/admin" component={AdminHome} />
+        <Route exact path="/admin-auth" component={AdminAuth} />
         <Route exact path="/blog/new" component={BlogNew} />
         <Route path="/blog/edit/:blogId" component={BlogEdit} />
       </Switch>
