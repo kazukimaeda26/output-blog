98e0be4e8c13f4d7500846aee7626f08858390a8
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-26 11:39:05 +0900

    詳細ページの作成

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -5,7 +5,7 @@ import AdminAuth from "./components/adminAuth/AdminAuth";
 import AdminHome from "./components/adminHome/AdminHome";
 import BlogNew from "./components/blogNew/BlogNew";
 import BlogEdit from "./components/blogEdit/BlogEdit";
-
+import BlogShow from "./components/blogShow/BlogShow";
 const Routing = () => {
   return (
     <BrowserRouter>
@@ -15,6 +15,7 @@ const Routing = () => {
         <Route exact path="/admin-home" component={AdminHome} />
         <Route exact path="/blog/new" component={BlogNew} />
         <Route path="/blog/edit/:blogId" component={BlogEdit} />
+        <Route path="/blog/:blogId" component={BlogShow} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/blogShow/BlogShow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogShow = () => {
+  return <div>aaaa</div>;
+};
+
+export default BlogShow;
